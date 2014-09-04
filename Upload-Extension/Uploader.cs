f26444d6eae11e262894ef7bb65b86a002b58e90
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-09-04 19:38:44 +0400

    Tiny cleanup

--- Upload-Extension/Uploader.cs
+++ Upload-Extension/Uploader.cs
@@ -13,12 +13,12 @@ namespace TRIK.Upload_Extension
 {
     public class Uploader
     {
-        private Dictionary<string, DateTime> lastUploaded = new Dictionary<string, DateTime>();
-        private ScpClient scpClient;
-        private SshClient sshClient;
-        private Timer timer = new Timer(5000.0);
-        private string projectPath = "";
-        private string projectName = "";
+        Dictionary<string, DateTime> lastUploaded = new Dictionary<string, DateTime>();
+        ScpClient scpClient;
+        SshClient sshClient;
+        Timer timer = new Timer(5000.0);
+        string projectPath = "";
+        string projectName = "";
         
         public Uploader(string ip)
         {
@@ -30,8 +30,8 @@ namespace TRIK.Upload_Extension
             sshClient.Connect();
             sshClient.KeepAliveInterval = TimeSpan.FromSeconds(10.0);
             
-            this.timer.Start();
-            this.timer.Elapsed += keepAlive;
+            timer.Start();
+            timer.Elapsed += keepAlive;
             sshClient.RunCommand("mkdir /home/root/trik-sharp; mkdir /home/root/trik-sharp/uploads");
         }
 
@@ -56,32 +56,40 @@ namespace TRIK.Upload_Extension
         private void UpdateScript()
         {
             var fullName = "/home/root/trik-sharp/" + projectName;
+            var pe = from file in Directory.GetFiles(projectPath) 
+                     where file.EndsWith(".exe") 
+                     select getName(file);
+
             var script =
                    "echo \"#!/bin/sh\n"
                 + "#killall trikGui\n"
-                + "mono " + getUploadPath(projectName) + ".exe $@ \n"
+                + "mono " + getUploadPath(pe.First()) + " $* \n"
                 + "cd ~/trik/\n"
                 + "#./trikGui -qws &> /dev/null &"
                 + "#some other commands\""
                 + " > " + fullName
                 + "; chmod +x " + fullName;
+            var stream = new System.IO.FileStream(projectPath + 
+                                                  pe.First().TrimSuffix(".exe"),
+                                                  FileMode.OpenOrCreate);
+            var bytes = Encoding.UTF8.GetBytes(script);
+            stream.Write(bytes, 0, bytes.Length);
             sshClient.RunCommand(script);
         }
 
         public void Update() 
         {
-        var newFiles = Directory.GetFiles(this.ProjectPath);
+        var newFiles = Directory.GetFiles(ProjectPath);
         foreach (var file in newFiles)
             {
-                if (!this.lastUploaded.ContainsKey(file))
+                if (!lastUploaded.ContainsKey(file))
                 {
-                        this.lastUploaded.Add(file, DateTime.MinValue);
+                        lastUploaded.Add(file, DateTime.MinValue);
                 }
                 var info = new FileInfo(file);
                 if (info.LastWriteTime > lastUploaded[file])
                 {
                     lastUploaded[file] = info.LastWriteTime;
-                    Console.WriteLine("uploading {0}", file);
                     scpClient.Upload(info, getUploadPath(file));
 
                 }
@@ -103,8 +111,7 @@ namespace TRIK.Upload_Extension
                     projectName = newProjectName;
                     lastUploaded.Clear();
                     sshClient.RunCommand("mkdir " + getUploadPath(""));
-                    Console.WriteLine("Updating Assembly name to {0}", value);
-                    this.UpdateScript();
+                    UpdateScript();
                     var libconwrap =
                     @"C:\Users\Alexander\Documents\GitHub\Trik-Observable\Source\BinaryComponents\libconWrap.so.1.0.0";
                     scpClient.Upload(new FileInfo(libconwrap), getUploadPath(libconwrap));
